b47e69ee68c3e1366551daaa7927ec39bcabd91d
Ricardo Alberto Escobar Gallardo <ricardoe489@gmail.com>
2026-07-27 13:23:14 -0400

    Descarga de excel Blob

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "896727915" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2335964998" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
